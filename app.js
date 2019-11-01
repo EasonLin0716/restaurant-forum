3b888c2bf67a7559ac2437ef5495c0de84598a1e
Eason Lin <uu9924079@gmail.com>
2019-11-01 16:47:49 +0800

    add disabled to user admin

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const passport = require('./config/passport')
 const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
+const Handlebars = require('handlebars')
+Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+  return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
+});
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
