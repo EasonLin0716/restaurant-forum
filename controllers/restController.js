78e7c6206005d8caec9889451da7a9a40657a780
Eason Lin <uu9924079@gmail.com>
2019-11-04 20:43:21 +0800

    Add restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,8 +1,19 @@
 // 負責處理前台餐廳相關的 request
-let resController = {
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+let restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    Restaurant.findAll({ include: Category }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        // 展開餐廳資料
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50)
+      }))
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
   }
 }
-
-module.exports = resController
+module.exports = restController
