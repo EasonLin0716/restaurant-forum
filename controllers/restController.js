c6f69f856e3b99e4b8f7f70034e67ba21410934c
Eason Lin <uu9924079@gmail.com>
2019-11-06 15:41:06 +0800

    Add dashboard page

--- controllers/restController.js
+++ controllers/restController.js
@@ -74,6 +74,12 @@ let restController = {
         })
       })
     })
+  },
+  getDashboard: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { include: [Category, Comment] })
+      .then(restaurant => {
+        res.render('dashboard', { restaurant })
+      })
   }
 }
 module.exports = restController
