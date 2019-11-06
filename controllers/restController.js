34083d0dc3a6905f7b5a4e7169cd17f97c84560d
Eason Lin <uu9924079@gmail.com>
2019-11-06 15:02:54 +0800

    add feeds

--- controllers/restController.js
+++ controllers/restController.js
@@ -56,6 +56,24 @@ let restController = {
         restaurant
       })
     })
+  },
+  getFeeds: (req, res) => {
+    return Restaurant.findAll({
+      limit: 10,
+      order: [['createdAt', 'DESC']], // 依 createdAt 降冪排序
+      include: [Category]
+    }).then(restaurants => {
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant]
+      }).then(comments => {
+        return res.render('feeds', {
+          restaurants,
+          comments
+        })
+      })
+    })
   }
 }
 module.exports = restController
