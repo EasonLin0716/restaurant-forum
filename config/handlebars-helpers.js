7d757c841f46837c2e7c02c1fccf21fa5d163edb
Eason Lin <uu9924079@gmail.com>
2019-11-07 15:03:15 +0800

    add ifNotEquals helper and user cannot follow himself

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -4,6 +4,9 @@ module.exports = {
   'ifEquals': function (arg1, arg2, options) {
     return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
   },
+  'ifNotEquals': function (arg1, arg2, options) {
+    return (arg1 !== arg2) ? options.fn(this) : options.inverse(this);
+  },
   moment: function (a) { // 呼叫 moment 並傳入一個時間參數 a
     return moment(a).fromNow()
   }
