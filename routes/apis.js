b0f7354ac77ea27fe56ed4f65c195449387e2ef8
Eason Lin <uu9924079@gmail.com>
2019-11-12 14:57:17 +0800

    add authenticated and authenticatedAdmin

--- routes/apis.js
+++ routes/apis.js
@@ -7,7 +7,19 @@ const userController = require('../controllers/api/userController.js')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 
-router.get('/admin/restaurants', adminController.getRestaurants)
+const passport = require('../config/passport')
+const authenticated = passport.authenticate('jwt', { session: false })
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) { return next() }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
+
+router.get('/admin/restaurants', authenticated, authenticatedAdmin, adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
