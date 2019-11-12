647d92baae687cdc6f7436b8943b04e01c894b74
Eason Lin <uu9924079@gmail.com>
2019-11-12 12:08:08 +0800

    add PUT /api/admin/categories/:id

--- services/categoryService.js
+++ services/categoryService.js
@@ -4,9 +4,17 @@ const Category = db.Category
 
 const categoryService = {
   getCategories: (req, res, callback) => {
-    return Category.findAll().then(categories => {
-      callback({ categories })
-    })
+    return Category.findAll()
+      .then(categories => {
+        if (req.params.id) {
+          Category.findByPk(req.params.id)
+            .then((category) => {
+              callback({ categories, category })
+            })
+        } else {
+          callback({ categories })
+        }
+      })
   },
   postCategory: (req, res, callback) => {
     if (!req.body.name) {
@@ -18,7 +26,20 @@ const categoryService = {
         callback({ status: 'success', message: 'category was successfully created' })
       })
     }
-  }
+  },
+  putCategory: (req, res, callback) => {
+    if (!req.body.name) {
+      callback({ status: 'error', message: 'name didn\'t exist' })
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then((category) => {
+              callback({ status: 'success', message: 'category was successfully updated' })
+            })
+        })
+    }
+  },
 }
 
 module.exports = categoryService
